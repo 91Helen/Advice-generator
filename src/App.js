15924421a5002b61da4f810724610acbf9fe0b5f
Helen <filatovae047@gmail.com>
2025-04-20 23:08:24 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import video from "./action.mp4" 
+
 
 function App() {
+const [advice, setAdvice] = useState(" ");
+
+
+
+useEffect(() => {
+getAdvice()
+}, [])
+  const getAdvice = async () => {
+    const response = await fetch(`https://bored.api.lewagon.com/api/activity `);
+    const data = await response.json();
+    setAdvice(data.activity);
+   
+  }
+ 
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app'>
+     <div>
+      <video autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+        </video>
+     </div>
+
+     <div className='card'>
+      <h1 className='heading'>{advice}</h1>
+ 
+      <button onClick={getAdvice} className='button'><span>Generate new advice</span></button></div>
+     
+      </div>
+
   );
 }
 
